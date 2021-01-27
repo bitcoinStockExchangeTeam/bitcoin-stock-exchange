2e814f91aa44e3f5be5205100ed9dd082748e183
mrozelek <mrozelek@gmail.com>
2021-01-27 13:52:27 +0100

    Add react router with example

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+export default class Home extends PureComponent {
+  render() {
+    return (
+      <p>
+        Hello from Home
+      </p>
+    );
+  }
+}
