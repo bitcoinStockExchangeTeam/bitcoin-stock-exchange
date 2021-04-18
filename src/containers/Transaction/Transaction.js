dbfad4bd1377c001e0d8cfeb710e43beef587b7c
unknown <nilphym@gmail.com>
2021-04-18 22:20:41 +0200

    Handle form validation

--- src/containers/Transaction/Transaction.js
+++ src/containers/Transaction/Transaction.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Switch from '@material-ui/core/Switch';
 import TextField from '@material-ui/core/TextField';
 import InputLabel from '@material-ui/core/InputLabel';
@@ -9,17 +9,48 @@ import Button from '@material-ui/core/Button';
 import { wrapper, heading, form, buttonContainer } from './transaction.module.scss';
 import Text from '../../components/Text';
 
-export default function Switches() {
-  const [checked, setChecked] = React.useState(true);
-  const [currency, setCurrency] = React.useState('');
+const Transaction = () => {
+  const [checked, setType] = useState(true);
+  const [amount, setAmount] = useState('');
+  const [amountProps, setAmountProps] = useState({ error: false, helperText: '' });
+  const [currenciesProps, setCurrenciesProps] = useState({ error: false });
+  const [currency, setCurrency] = useState('');
+  const [currencies, setCurrencies] = useState([]);
 
-  const handleCheckedChange = () => {
-    setChecked(!checked);
+  const handleTypeChange = () => {
+    setType((checkedPrev) => !checkedPrev);
   };
 
-  const handleNameChange = (event) => {
+  const handleCurrencyChange = (event) => {
+    if (event.target.value === '') {
+      setCurrenciesProps({ error: true });
+    } else {
+      setCurrenciesProps({ error: false });
+    }
     setCurrency(event.target.value);
   };
+
+  const handleAmountChange = (event) => {
+    const regexDecimal = /^\d+\.\d{0,2}$/;
+    const regexInt = /^\d+$/;
+
+    if (regexDecimal.test(event.target.value) || regexInt.test(event.target.value)) {
+      setAmountProps({ error: false, helperText: '' });
+    } else {
+      setAmountProps({ error: true, helperText: 'Input must be a number.' });
+    }
+    setAmount(event.target.value);
+  };
+
+  const handleConfirmButton = () => {
+    if (currency === '') {
+      setCurrenciesProps({ error: true });
+    }
+  };
+
+  useEffect(() => {
+    setCurrencies(['BTC', 'ETH', 'USDT', 'BNB']);
+  }, []);
 
   return (
     <div className={wrapper}>
@@ -28,7 +59,7 @@ export default function Switches() {
         <Text text={checked ? 'Buy' : 'Sell'} type="HEADING_5" state={checked ? 'SUCCESS' : 'ACCENT'} />
         <Switch
           checked={checked}
-          onChange={handleCheckedChange}
+          onChange={handleTypeChange}
           color="primary"
           name="transactionType"
           inputProps={{ 'aria-label': 'primary checkbox' }}
@@ -41,33 +72,36 @@ export default function Switches() {
             labelId="currencies"
             id="currencies"
             value={currency}
-            onChange={handleNameChange}
+            onChange={handleCurrencyChange}
             label="Name"
+            {...currenciesProps}
           >
-            <MenuItem value="BTC">BTC</MenuItem>
-            <MenuItem value="ETH">ETH</MenuItem>
-            <MenuItem value="USDT">USDT</MenuItem>
-            <MenuItem value="BNB">BNB</MenuItem>
+            {currencies.map((currencyItem) => (
+              <MenuItem key={currencyItem} value={currencyItem}>{currencyItem}</MenuItem>))}
           </Select>
         </FormControl>
         <TextField
           id="price"
           label="Price"
           variant="outlined"
+          disabled
         />
         <TextField
           id="amount"
           label="Amount"
           variant="outlined"
+          onChange={handleAmountChange}
+          {...amountProps}
         />
         <TextField
           id="total"
           label="Total"
           variant="outlined"
+          disabled
         />
       </div>
       <div className={buttonContainer}>
-        <Button variant="contained" color="primary">
+        <Button variant="contained" color="primary" onClick={handleConfirmButton}>
           Confirm
         </Button>
         <Button variant="outlined" color="primary">
@@ -76,4 +110,6 @@ export default function Switches() {
       </div>
     </div>
   );
-}
+};
+
+export default Transaction;
