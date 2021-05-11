825c65a79c2b16a27636f0bad481fd4365562387
unknown <nilphym@gmail.com>
2021-05-11 15:37:41 +0200

    Add tests for switch and reset button

--- src/containers/Transaction/Transaction.test.js
+++ src/containers/Transaction/Transaction.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { render, screen, fireEvent, act } from '@testing-library/react';
+import { render, screen, fireEvent, act, within } from '@testing-library/react';
 import Transaction, { errorMessages } from './Transaction';
 import useStockExchangeData from '../../hooks/useStockExchangeData';
 
 describe('Transaction', () => {
+  const stockExchangeData = useStockExchangeData();
+
   beforeEach(() => {
-    const stockExchangeData = useStockExchangeData();
     render(<Transaction stockExchangeData={stockExchangeData} />);
   });
 
@@ -36,5 +37,25 @@ describe('Transaction', () => {
       fireEvent.submit(screen.getByRole('button', { name: 'Confirm' }));
     });
     expect(screen.getByText(errorMessages.integer)).toBeInTheDocument();
+  });
+
+  it('should change transaction type when switch for transaction type is clicked', async () => {
+    const prevValue = screen.getByRole('checkbox').value;
+    await act(async () => {
+      fireEvent.click(screen.getByRole('checkbox'));
+    });
+    expect(screen.getByRole('checkbox').value).not.toBe(prevValue);
+  });
+
+  it('should reset form when reset button is clicked', async () => {
+    await act(async () => {
+      fireEvent.input(screen.getByLabelText('Amount'), { target: { value: 15.3 } });
+    });
+    expect(screen.getByLabelText('Amount').value).toBe('15.3');
+
+    await act(async () => {
+      fireEvent.click(screen.getByRole('button', { name: 'Reset' }));
+    });
+    expect(screen.getByLabelText('Amount').value).toBe('0');
   });
 });
