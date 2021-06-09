7eb5fd306b1b221fe8d58b6a1f1f67ccde6d089a
unknown <nilphym@gmail.com>
2021-06-09 16:00:58 +0200

    Get data from api in main component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import SiteHeading from './containers/SiteHeading';
 import MarketPage from './containers/MarketPage';
 import { wrapper } from './App.module.scss';
+import { getTickers } from './redux/reducers/stock/actions';
 
-const App = () => (
-  <div className={wrapper}>
-    <SiteHeading />
-    <MarketPage />
-  </div>
-);
+const App = () => {
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(getTickers());
+  }, []);
+
+  return (
+    <div className={wrapper}>
+      <SiteHeading />
+      <MarketPage />
+    </div>
+  );
+};
 
 export default App;
