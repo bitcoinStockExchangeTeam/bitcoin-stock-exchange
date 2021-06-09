1c8bb40f46f440972a5b923da3a3543a88e065e3
unknown <nilphym@gmail.com>
2021-06-09 23:42:33 +0200

    Handle errors and loading states for CurrencyTable

--- src/components/CurrencyTable/index.js
+++ src/components/CurrencyTable/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import CurrencyTable from './CurrencyTable';
+import Text from '../Text';
+
+const useStyles = makeStyles(() => ({
+  center: {
+    display: 'flex',
+    justifyContent: 'center'
+  }
+}));
 
 const CurrencyTableWrapper = () => {
   const stockState = useSelector((state) => state.stock);
+  const classes = useStyles();
+
+  if (stockState.isLoading) {
+    return <div className={classes.center}><CircularProgress /></div>;
+  }
+
+  if (stockState.isError) {
+    return (
+      <div className={classes.center}>
+        <Text text="There was error when connecting to API. Please wait a couple of seconds and refresh the page." type="HEADING_4" state="ERROR" />
+      </div>
+    );
+  }
+
   return <CurrencyTable stockExchangeData={stockState.stockData} />;
 };
 
